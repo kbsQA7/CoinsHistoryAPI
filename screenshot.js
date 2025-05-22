8564011f2e8b248741592cf32906e9189246a512
Kirill Babanin <154428688+kbsQA7@users.noreply.github.com>
2025-05-23 04:48:09 +0500

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -1,47 +1,34 @@
-
-
 import puppeteer from 'puppeteer';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { existsSync } from 'fs';
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-
-const reportPath = path.resolve(__dirname, 'build', 'reports', 'allure-report', 'allureReport', 'index.html');
-
-
+const reportPath = path.resolve(__dirname, 'build', 'reports', 'allure-report', 'index.html');
 
 if (!existsSync(reportPath)) {
   console.error('❌ Allure report index.html not found. Make sure it is generated before running this script.');
   process.exit(1);
 }
 
-
 const htmlFileUrl = `file://${reportPath}`;
-
-
 const screenshotPath = path.resolve(__dirname, 'allure-summary.png');
-
 
 const browser = await puppeteer.launch({
   headless: 'new',
-  defaultViewport: { width: 1400, height: 1000 },
-  args: ['--no-sandbox', '--disable-setuid-sandbox'] 
+  args: ['--no-sandbox'], // важно для GitHub Actions
+  defaultViewport: { width: 1400, height: 1000 }
 });
 
 const page = await browser.newPage();
-
 await page.goto(htmlFileUrl, { waitUntil: 'networkidle0' });
 
-// ⏳ Ждём 3 секунды, чтобы Allure успел отрисовать метрики
-await page.waitForTimeout(4000);
+// ⏳ Ждём вручную 4 секунды
+await new Promise(resolve => setTimeout(resolve, 4000));
 
 await page.screenshot({ path: screenshotPath });
-
-
 await browser.close();
 
 console.log(`✅ Screenshot saved to ${screenshotPath}`);
+
