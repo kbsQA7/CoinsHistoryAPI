e85a0692566a10816040616d2ef58f8d495881d4
Kirill Babanin <154428688+kbsQA7@users.noreply.github.com>
2025-05-23 04:10:50 +0500

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -1,3 +1,5 @@
+
+
 import puppeteer from 'puppeteer';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -6,17 +8,27 @@ import { existsSync } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+
 const reportPath = path.resolve(__dirname, 'build', 'reports', 'allure-report', 'index.html');
+
 
 if (!existsSync(reportPath)) {
   console.error('❌ Allure report index.html not found. Make sure it is generated before running this script.');
   process.exit(1);
 }
 
+
 const htmlFileUrl = `file://${reportPath}`;
+
+
 const screenshotPath = path.resolve(__dirname, 'allure-summary.png');
 
-const browser = await puppeteer.launch({ headless: 'new', defaultViewport: { width: 1400, height: 1000 } });
+
+const browser = await puppeteer.launch({
+  headless: 'new',
+  defaultViewport: { width: 1400, height: 1000 }
+});
 const page = await browser.newPage();
 
 await page.goto(htmlFileUrl, { waitUntil: 'networkidle0' });
